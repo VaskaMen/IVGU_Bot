594cc2ba9201b6b1e9e38cb7671f91fc08ce467b
Sivokk1 <savelyi.golutvins@gmail.com>
2025-02-22 16:41:06 +0300

    Teacher_place, time

--- IVGU/Table/TableObjects/Cell.py
+++ IVGU/Table/TableObjects/Cell.py
@@ -1,46 +1,72 @@
+import re
+
 from bs4 import BeautifulSoup, ResultSet, Tag
+
+from IVGU.ScheduleObject.TeacherPlace import TeacherPlace
 
 
 class Cell:
     def get_subject_name_from_cell(self,cell: str) -> str:
-        lesson = self.get_raw_subject(cell)
+        lesson = self.__get_raw_subject(cell)
         lesson_name = str(lesson[0]).replace('<','>').split('>')[2].split('(')[0]
         return lesson_name
 
     def get_subject_type_from_cell(self,cell: str) -> str:
-        lesson = self.get_raw_subject(cell)
+        lesson = self.__get_raw_subject(cell)
         type_lesson = str(lesson[0]).replace('<','>').split('>')[2].split('(')[1].replace(")", "")
         return type_lesson
 
     @staticmethod
-    def get_raw_subject(cell: str) -> ResultSet[Tag]:
+    def __get_raw_subject(cell: str) -> ResultSet[Tag]:
         lesson = BeautifulSoup(cell, 'html.parser').select('.text-bold')
         return lesson
 
-    def get_teacher_from_cell(self,cell: str) -> str:
-        teacher_cell = self.get_raw_teacher(cell)
-        teacher = str(teacher_cell[0]).replace('<','>').split('>')[2].split(',')[0]
+    def get_teacher_from_cell(self,cell: str) -> list[TeacherPlace]:
+        teacher_cell = self.__get_raw_teacher(cell)
+        all_teach_place = []
+        for i in teacher_cell:
+            teacher = self.__get_teacher_name(str(i))
+            place = self.__get_place(str(i))
+            teach_place = TeacherPlace(teacher,place)
+            all_teach_place.append(teach_place)
+        return all_teach_place
+
+    @staticmethod
+    def __get_teacher_name(teacher_cell: str):
+        teacher = teacher_cell.replace('<','>').split('>')[2].split(',')[0]
         return teacher
 
     @staticmethod
-    def get_raw_teacher(cell: str) -> ResultSet[Tag]:
-        teacher_cell = BeautifulSoup(cell, 'html.parser').select('.white-space-nowrap i')
-        return teacher_cell
+    def __get_place(teacher_cell:str):
+        place = teacher_cell.replace('<','>').split('>')[2].split(',')[1]
+        return place.lstrip()
 
-    def get_place_from_cell(self,cell: str) -> str:
-        place_cell = self.get_raw_teacher(cell)
-        place = str(place_cell[0]).replace('<','>').split('>')[2].split(',')[1].lstrip()
-        return place
+    def __get_raw_teacher(self,cell: str) -> list[Tag]:
+        teacher_cell = BeautifulSoup(cell, 'html.parser').select('.white-space-nowrap i')
+        clean_teacher_cell = self.__clean_teachers(teacher_cell)
+        return clean_teacher_cell
+
+    def __clean_teachers(self,teacher_cells:ResultSet[Tag]) -> list[Tag]:
+        clean_teachers = []
+        for i in teacher_cells:
+            if self.have_digits(i.text):
+                clean_teachers.append(i)
+        return clean_teachers
+
+    @staticmethod
+    def have_digits(string: str) ->bool:
+        return len(re.split(r"\d",string)) != 1
+
 
     def get_data_date_from_cell(self,cell: str):
-        data = self.get_raw_data(cell)
+        data = self.__get_raw_data(cell)
         return data.get("data-date")
 
     @staticmethod
-    def get_raw_data(cell: str):
+    def __get_raw_data(cell: str) -> Tag:
         data = BeautifulSoup(str(cell), 'html.parser').select(".cell")[0]
         return data
 
     def get_data_time_from_cell(self,cell: str):
-        data = data = self.get_raw_data(cell)
+        data = data = self.__get_raw_data(cell)
         return data.get("data-time")
--- test.py
+++ test.py
@@ -2,38 +2,34 @@ from bs4 import BeautifulSoup
 import Seecret
 from IVGU.Ivgu import Ivgu
 from IVGU.Table.TableConvertor import TableConvertor
-
 tbc = TableConvertor()
 ivgu = Ivgu()
 ivgu.login(Seecret.IVGU_LOGIN, Seecret.IVGU_PASSWORD)
+
 page = ivgu.get_schedule_page(118, 6, 1, 1, 2)
+tables = tbc.get_subject_tables(page)
+lines = tbc.line.get_lines_of_subjects(tables[0])
+new = tbc.line.lines_separator_into_cells(lines)
 
-tables = tbc.get_time_table(page)
-tbody = tbc.get_tbody_of_times(tables[1])
+time_tables = tbc.get_time_table(page)
+tbody = tbc.get_tbody_of_times(time_tables[1])
+timecodes = tbc.get_time_codes(tbody)
+quantity = tbc.get_quantity_of_subgroups(tables[0])
+
+for i in new:
+    if i.text != "":
+        print(tbc.cell.get_subject_name_from_cell(str(i)))
+        print(tbc.cell.get_subject_type_from_cell(str(i)))
+        print(tbc.cell.get_teacher_from_cell(str(i)))
+        print(tbc.cell.get_data_date_from_cell(str(i)))
+        print(timecodes[tbc.cell.get_data_time_from_cell(str(i))])
+        print("")
 
 
 
-print(tbc.get_time_codes(tbody))
 
 
 
-
-
-
-
-
-
-
-
-
-
+# ivgu = Ivgu()
 
 # page = ivgu.get_schedule_page(118, 6, 1, 1, 2)
-# tables = tbc.get_subject_tables(page)
-# lines = tbc.line.get_lines_of_subjects(tables[0])
-# new = tbc.line.lines_separator_into_cells(lines)
-# n = 12
-# print(tbc.cell.get_subject_name_from_cell(str(new[n])))
-# print(tbc.cell.get_subject_type_from_cell(str(new[n])))
-# print(tbc.cell.get_teacher_from_cell(str(new[n])))
-# print(tbc.cell.get_place_from_cell(str(new[n])))
