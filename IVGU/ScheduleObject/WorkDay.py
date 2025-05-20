bd9a219b36641c88dc2d9ed45521c2bd660fccb2
Sivokk1 <savelyi.golutvins@gmail.com>
2025-05-20 20:10:16 +0300

    IVGU bot schedule Pre release 1.2 symbols

--- IVGU/ScheduleObject/WorkDay.py
+++ IVGU/ScheduleObject/WorkDay.py
@@ -20,8 +20,23 @@ class WorkDay:
     def __str__(self) -> str:
         if len(self.lessons) != 0:
             res = f"***{self.date} {week[self.date.weekday()]}***\n\n"
+
+            all_symbols = ['🔸','🔹','🔬','🖥', '📚']
             for lesson in self.lessons:
-                res += f"⌚  ***{lesson.time}*** \n📘  {lesson.name} \n🔹  ___{lesson.type_subject}___ \n"
+                if lesson.type_subject == "лекция":
+                    sym = all_symbols[1]
+                elif "лабораторн" in lesson.type_subject.lower():
+                    sym = all_symbols[2]
+                elif "практическ"  or "семинар" in lesson.type_subject.lower():
+                    sym = all_symbols[0]
+                elif lesson.type_subject == "дистанционное занятие":
+                    sym = all_symbols[3]
+                elif lesson.type_subject == "Факультатив":
+                    sym = all_symbols[4]
+                else:
+                    sym = all_symbols[1]
+
+                res += f"⌚  ***{lesson.time}*** \n📘  {lesson.name} \n{sym}  ___{lesson.type_subject}___ \n"
                 for teacher_place in lesson.teacher_places:
                     res += f"👨‍🏫  {teacher_place.teacher} \n🚪  ***{teacher_place.place}***\n"
                 res += "\n\n"
